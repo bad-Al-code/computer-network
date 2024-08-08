ea59b468797c87e4dbeafea5b7e3ff993b2a0736
badAl <badalsingh120403@gmail.com>
2024-08-08 23:22:42 +0530

    refactor: Update listener test to follow Go best practices

--- listen_test.go
+++ listen_test.go
@@ -7,51 +7,68 @@ import (
 )
 
 func TestListener(t *testing.T) {
-	listener, err := net.Listen("tcp", "127.0.0.1:o")
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("failed to create listener: %v", err)
 	}
+	defer func() {
+		if closeErr := listener.Close(); closeErr != nil {
+			t.Errorf("failed to close listener: %v", closeErr)
+		}
+	}()
 
 	done := make(chan struct{})
 
 	go func() {
-		defer func() { done <- struct{}{} }()
+		defer close(done)
 
 		for {
 			conn, err := listener.Accept()
 			if err != nil {
-				t.Log(err)
+				if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+					t.Log("listener timed out")
+				} else {
+					t.Errorf("failed to accept connection: %v", err)
+				}
 				return
 			}
 
-			go func(c net.Conn) {
-				defer func() {
-					c.Close()
-					done <- struct{}{}
-				}()
-
-				buf := make([]byte, 1024)
-				for {
-					n, err := c.Read(buf)
-					if err != nil {
-						if err != io.EOF {
-							t.Error(err)
-						}
-						return
-					}
-					t.Logf("received: %q", buf[:n])
-				}
-			}(conn)
+			go handleConnection(t, conn, done)
 		}
 	}()
 
 	conn, err := net.Dial("tcp", listener.Addr().String())
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("failed to connect: %v", err)
 	}
+	defer func() {
+		if closeErr := conn.Close(); closeErr != nil {
+			t.Errorf("failed to close connection: %v", closeErr)
+		}
+	}()
 
-	conn.Close()
 	<-done
-	listener.Close()
-	<-done
+}
+
+func handleConnection(t *testing.T, conn net.Conn, done chan<- struct{}) {
+	defer func() {
+		if closeErr := conn.Close(); closeErr != nil {
+			t.Errorf("failed to close connection: %v", closeErr)
+		}
+		done <- struct{}{}
+	}()
+
+	buf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				t.Log("connection closed by client")
+			} else {
+				t.Errorf("failed to read from connection: %v", err)
+			}
+			return
+		}
+		t.Logf("received: %q", buf[:n])
+	}
 }
